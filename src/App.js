0a9c3579d813cc0c5deccfc16fdcc33e55c0eece
Sujal Mandalia <82870049+SUJAL-MANDALIA@users.noreply.github.com>
2023-02-23 16:45:45 +0530

    Day 4 Task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Counter/>
     </div>
   );
 }
